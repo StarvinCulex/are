1d122352376a60b1dfa1220997aab3071c4e342c
8q <yggdfolium@gmail.com>
2022-04-05 14:18:22 +0800

    use from_entropy() instead of all-zero seed

--- are-world/src/arena/mind/gods/plant.rs
+++ are-world/src/arena/mind/gods/plant.rs
@@ -1,6 +1,4 @@
-use std::borrow::{Borrow, BorrowMut};
-
-use rand::distributions::Distribution;
+use rand::distributions::Uniform;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 
@@ -10,7 +8,7 @@ use crate::arena::{gnd, Cosmos, Orderer};
 use crate::Coord;
 
 pub struct GodOfPlant {
-    rng: rand::rngs::StdRng,
+    rng: StdRng,
 }
 
 impl GodOfPlant {
@@ -18,8 +16,7 @@ impl GodOfPlant {
 
     pub fn new() -> GodOfPlant {
         GodOfPlant {
-            // how to get seed?
-            rng: rand::rngs::StdRng::from_seed([0; 32]),
+            rng: StdRng::from_entropy(),
         }
     }
 }
@@ -33,12 +30,12 @@ impl Mind for GodOfPlant {
         let plate_distributes = cosmos
             .plate
             .size()
-            .map(|x| rand::distributions::Uniform::from(0..x));
+            .map(|x| Uniform::from(0..x));
         let area = cosmos.plate.size().0 * cosmos.plate.size().1;
 
         let aging_count = cosmos.angelos.properties.runtime_conf.plant_aging * area as f64;
         if aging_count >= 1.0 || {
-            let aging_distributes = rand::distributions::Uniform::from(0.0..1.0);
+            let aging_distributes = Uniform::from(0.0..1.0);
             let p = self.rng.sample(aging_distributes);
             p <= aging_count
         } {
@@ -55,12 +52,12 @@ impl Mind for GodOfPlant {
 
         let sow_count = (cosmos.angelos.properties.runtime_conf.plant_sow * area as f64).ceil();
         if sow_count >= 1.0 || {
-            let sow_distributes = rand::distributions::Uniform::from(0.0..1.0);
+            let sow_distributes = Uniform::from(0.0..1.0);
             let p = self.rng.sample(sow_distributes);
             p <= sow_count
         } {
             let kind_distributes =
-                rand::distributions::Uniform::from(0..GodOfPlant::KIND_LIST.len());
+                Uniform::from(0..GodOfPlant::KIND_LIST.len());
             for _ in 0..sow_count as usize {
                 let p = Coord(
                     self.rng.sample(plate_distributes.0),
