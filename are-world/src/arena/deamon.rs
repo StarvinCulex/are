059a6eff658069a25942dcc5e5b2374a69ef5030
8q <yggdfolium@gmail.com>
2022-05-01 15:14:56 +0800

    bug fix

--- are-world/src/arena/deamon.rs
+++ are-world/src/arena/deamon.rs
@@ -1,32 +1,39 @@
 //
 pub struct Deamon<'c> {
     pub angelos: Angelos<'c>,
-    plate: *mut Matrix<Block, 1, 1>,
+    plate: &'c mut Matrix<Block, 1, 1>,
     bound: CrdI,
 }
 
 impl<'c> Deamon<'c> {
-    pub fn new(cosmos: &'c Cosmos, bound: CrdI) -> Self {
+    pub fn new(cosmos: &'c mut Cosmos, bound: CrdI) -> Self {
         Self {
             angelos: cosmos.angelos.make_worker(),
-            plate: &cosmos.plate as *const _ as *mut _,
+            plate: &mut cosmos.plate,
             bound,
         }
     }
 
-    pub fn by_divide(cosmos: &'c Cosmos, divide: (Idx, Idx)) -> Vec<Self> {
+    /// 把 cosmos 分成 divide.0 * divide.1 块
+    pub fn by_divide(cosmos: &'c mut Cosmos, divide: (Idx, Idx)) -> Vec<Self> {
         let plate_size: Coord<Idx> = cosmos.angelos.plate_size.try_into().unwrap();
-        assert!(plate_size.0 % divide.0 == 0 && plate_size.1 % divide.1 == 0);
-        let divide = (plate_size.0 / divide.0, plate_size.1 / divide.1);
-        assert!(divide.0 >= 3 && divide.1 >= 3);
-        let chunks = Vec::with_capacity((divide.0 * divide.1) as usize);
+        debug_assert!(plate_size.0 % divide.0 == 0 && plate_size.1 % divide.1 == 0);
+        debug_assert!(divide.0 >= 3 && divide.1 >= 3);
+        let mut chunks = Vec::with_capacity((divide.0 * divide.1) as usize);
         for i in 0..divide.0 {
             for j in 0..divide.1 {
                 let bound = Coord(i * divide.0, j * divide.1) | Coord((i + 1) * divide.0 - 1, (j + 1) * divide.1 - 1);
-                chunks.push(Self::new(cosmos, bound));
+                chunks.push(Self::new(unsafe { std::mem::transmute(&cosmos) }, bound));
             }
         }
         chunks
+    }
+
+    /// 把 cosmos 以 chunk_size.0 * chunk_size.1 分块
+    pub fn by_chunk_size(cosmos: &'c mut Cosmos, chunk_size: (Idx, Idx)) -> Vec<Self> {
+        let plate_size: Coord<Idx> = cosmos.angelos.plate_size.try_into().unwrap();
+        debug_assert!(plate_size.0 % chunk_size.0 == 0 && plate_size.1 % chunk_size.1 == 0);
+        Self::by_divide(cosmos, (plate_size.0 / chunk_size.0, plate_size.1 / chunk_size.1))
     }
 
     pub fn set(
@@ -37,30 +44,28 @@ impl<'c> Deamon<'c> {
         if !self.contains(at) {
             return Err(mob);
         }
-        let mut plate = unsafe { &mut *self.plate };
 
         // check if the plate is empty
-        if plate.area(at).scan().any(|(_, grid)| grid.mob.is_some()) {
+        if self.plate.area(at).scan().any(|(_, grid)| grid.mob.is_some()) {
             return Err(mob);
         }
         // set the plate
         let mob: P<MobBlock> = mob.into();
-        for (_, grid) in plate.area_mut(at) {
+        for (_, grid) in self.plate.area_mut(at) {
             grid.mob = Some(mob.clone());
         }
         Ok(mob)
     }
 
     pub fn take(&mut self, mob: Weak<MobBlock>) -> Result<ArcBox<MobBlock>, ()> {
-        let mut plate = unsafe { &mut *self.plate };
         let mob = mob.upgrade().ok_or(())?;
         let at = mob.at();
 
         // check if the mob.at() is valid
-        if &mob != plate[at.from()].mob.as_ref().ok_or(())? {
+        if &mob != self.plate[at.from()].mob.as_ref().ok_or(())? {
             return Err(());
         }
-        let scan = plate.area_mut(at).scan();
+        let scan = self.plate.area_mut(at).scan();
         // quick fail: check if it's unique after clearing the plate
         if scan.len() + 1 < mob.strong_count() {
             return Err(());
@@ -80,11 +85,10 @@ impl<'c> Deamon<'c> {
             return Err(());
         }
         // lock() before upgrade(), as it can be take()-n between upgrade() and lock() otherwise
-        let mut plate = unsafe { &mut *self.plate };
         let mut mob = mob.upgrade().ok_or(())?;
         let at = mob.at();
         // check if there is another mob
-        if plate
+        if self.plate
             .area(at)
             .scan()
             .any(|(_, grid)| grid.mob.is_some_with(|pos_mob| pos_mob != &mob))
@@ -92,13 +96,13 @@ impl<'c> Deamon<'c> {
             return Err(());
         }
         // clear the old grids
-        for (pos, grid) in plate.area_mut(at) {
+        for (pos, grid) in self.plate.area_mut(at) {
             if !new_at.contains(&pos.try_into().unwrap()) {
                 grid.mob = None;
             }
         }
         // set the new grids
-        for (_, grid) in plate.area_mut(new_at) {
+        for (_, grid) in self.plate.area_mut(new_at) {
             if grid.mob.is_none() {
                 grid.mob = Some(mob.clone());
             }
